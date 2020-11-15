7be9d78c06aa9836ac3a1345cf4af5f5792898fd
Code-Engraver <codeengraver@gmail.com>
2020-11-15 15:14:33 +0900

    ADD - Comment Models 추가
    Comment 앱에 Models 추가 완료. Comment, Reply 구축

--- comment/models.py
+++ comment/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Comment(models.Model):
+    nickname = models.CharField(max_length=7, null=False, blank=False)
+    password = models.CharField(max_length=255, null=False, blank=False)
+    content = models.CharField(max_length=120, null=False, blank=False)
+    create_date = models.DateTimeField(auto_now_add=True)
+    update_date = models.DateTimeField(auto_now=True)
+
+
+class Reply(models.Model):
+    comment = models.ForeignKey(Comment, on_delete=models.CASCADE)
+    nickname = models.CharField(max_length=7, null=False, blank=False)
+    password = models.CharField(max_length=255, null=False, blank=False)
+    content = models.CharField(max_length=120, null=False, blank=False)
+    create_date = models.DateTimeField(auto_now_add=True)
+    update_date = models.DateTimeField(auto_now=True)
