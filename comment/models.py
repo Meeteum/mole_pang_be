507edeb2d0131d1bd53668deace1ab11ec100e15
Code-Engraver <codeengraver@gmail.com>
2020-11-15 15:37:22 +0900

    MOD - Comment Models 수정
    Comment Models를 수정합니다. Meta를 추가합니다.

--- comment/models.py
+++ comment/models.py
@@ -8,6 +8,13 @@ class Comment(models.Model):
     create_date = models.DateTimeField(auto_now_add=True)
     update_date = models.DateTimeField(auto_now=True)
 
+    class Meta:
+        app_label = "comment"
+        db_table = "comment"
+        verbose_name = "comment"
+        verbose_name_plural = "comments"
+        ordering = ['-create_date', ]
+
 
 class Reply(models.Model):
     comment = models.ForeignKey(Comment, on_delete=models.CASCADE)
@@ -16,3 +23,10 @@ class Reply(models.Model):
     content = models.CharField(max_length=120, null=False, blank=False)
     create_date = models.DateTimeField(auto_now_add=True)
     update_date = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        app_label = "comment"
+        db_table = "reply"
+        verbose_name = "reply"
+        verbose_name_plural = "replies"
+        ordering = ['-create_date', ]
