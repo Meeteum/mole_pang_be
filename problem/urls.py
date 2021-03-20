bcde7d9dde6b5cf0c574fec74c73387350fa3d1d
code-engraver <codeengraver@gmail.com>
2021-03-21 00:19:21 +0900

    문제 리스트 개발 완료

--- problem/urls.py
+++ problem/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import InitProblemData
+from .views import InitProblemData, ProblemList
 
 urlpatterns = [
+    path('', ProblemList.as_view()),
     path('init_problem_data/', InitProblemData.as_view()),
 ]
--- problem/views.py
+++ problem/views.py
@@ -6,9 +6,81 @@ from .serializers import ProblemSerializer
 import os
 from django.conf import settings
 import pandas as pd
+import random
+
+
+class ProblemList(APIView):
+    # noinspection PyMethodMayBeStatic
+    def get(self, request):
+        """
+            문제 리스트
+
+            ---
+            ### url: /problem/
+            ### method: GET
+            ### body request: 없음
+            ### 문제 객체
+                {
+                    "stage":1,
+                    "consonant": "ㄱㅁㅎ",
+                    "problem": ["바나나", "고릴라", "만약에", "가만히"],
+                    "answer": 3,
+                    "meaning": "찌그러져 있다"
+                }
+            ### 반환값: 문제 객체 리스트
+        """
+        def get_problem(target_df, consonant):
+            correct_word_df = target_df[target_df['consonant'] == consonant].sample()
+            wrong_word_df = target_df[target_df['consonant'] != consonant].sample(n=3)
+
+            problem_list = correct_word_df[['word', 'consonant']].values.tolist()
+            problem_list.extend(wrong_word_df[['word', 'consonant']].values.tolist())
+
+            random.shuffle(problem_list)
+
+            problem = {
+                'consonant': consonant,
+                'problem': [lst[0] for lst in problem_list],
+                'answer': problem_list.index(correct_word_df[['word', 'consonant']].values.tolist()[0]),
+                'meaning': correct_word_df['meaning'].values[0]
+            }
+
+            return problem
+
+        def get_consonant_list(target_df, length):
+            return random.sample(list(set(target_df['consonant'])), length)
+
+        result = list()
+
+        df = pd.DataFrame(list(Problem.objects.all().values()))
+
+        one_df = df[df['length'] == 1]
+        for one_consonant in get_consonant_list(one_df, 10):
+            result.append(get_problem(one_df, one_consonant))
+
+        two_df = df[df['length'] == 2]
+        for two_consonant in get_consonant_list(two_df, 18):
+            result.append(get_problem(two_df, two_consonant))
+
+        three_df = df[df['length'] == 3]
+        for three_consonant in get_consonant_list(three_df, 17):
+            result.append(get_problem(three_df, three_consonant))
+
+        four_df = df[df['length'] == 4]
+        for four_consonant in get_consonant_list(four_df, 5):
+            result.append(get_problem(four_df, four_consonant))
+
+        random.shuffle(result)
+
+        for index, r in enumerate(result):
+            r['stage'] = index + 1
+
+        return Response(result)
 
 
 class InitProblemData(APIView):
+    swagger_schema = None
+
     # noinspection PyMethodMayBeStatic
     def post(self, request):
         try:
