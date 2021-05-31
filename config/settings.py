6f99e9bd764771ee1fbe77522fa499652af13746
WonSeok An <won@weinteract.co>
2021-05-31 21:59:08 +0900

    add - add deploy url

--- config/settings.py
+++ config/settings.py
@@ -135,5 +135,6 @@ CORS_ALLOW_CREDENTIALS = True
 CORS_ORIGIN_ALLOW_ALL = False
 CORS_ORIGIN_WHITELIST = [
     'http://127.0.0.1:3000',
-    'http://localhost:3000'
+    'http://localhost:3000',
+    'https://yami-yun.github.io/'
 ]
